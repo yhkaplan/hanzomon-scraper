b4b86461ed8ad76de02ed7dbd07c0825f7f79bf2
Joshua Kaplan <yhkaplan@gmail.com>
2019-02-15 21:11:11 +0900

    Make slackbot package

---
+++ slackbot/slackbot.go
@@ -0,0 +1,16 @@
+package slackbot
+
+import (
+	"os"
+
+	"github.com/nlopes/slack"
+)
+
+func SendMessage(message string, channel string) {
+	token := os.Getenv("SLACK_TOKEN")
+	api := slack.New(token)
+	rtm := api.NewRTM()
+
+	msg := rtm.NewOutgoingMessage(message, channel)
+	rtm.SendMessage(msg)
+}
