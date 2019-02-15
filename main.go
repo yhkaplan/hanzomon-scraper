1e6638001c848865a750dfb8f568555c3579d6c8
Joshua Kaplan <yhkaplan@gmail.com>
2019-02-15 20:38:25 +0900

    Implement basic web scraping for today's announcement

--- main.go
+++ main.go
@@ -1,35 +1,64 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
-	client := &http.Client{
-		Timeout: 30 * time.Second,
-	}
-
-	req, err := http.NewRequest("GET", "https://www.devdungeon.com", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	req.Header.Set("User-Agent", "Firefox")
-
-	resp, err := client.Do(req)
+	resp, err := http.Get("https://www.tokyometro.jp/unkou/history/hanzoumon.html")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
 
-	// Copy data from resp to stdout
-	body, err := io.Copy(os.Stdout, resp.Body)
+	// Create goquery doc
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println("Num:", body)
+	doc.Find("tr").Each(processTableRow)
+}
+
+func processTableRow(index int, tableRow *goquery.Selection) {
+	txt := tableRow.Text()
+	announce := announcementMessage(txt)
+
+	date, err := currentLocalizedDate()
+	if err != nil {
+		fmt.Printf("Fatal error: %s", err)
+		os.Exit(1)
+	}
+
+	isToday := strings.Contains(txt, date)
+	if isToday {
+		fmt.Printf("%s\n\n", announce)
+	}
+}
+
+// print text after 分, stripping newlines and whitespace
+func announcementMessage(txt string) string {
+	messageStartIdx := strings.Index(txt, "分") + 4
+	message := string([]byte(txt[messageStartIdx:]))
+
+	return strings.TrimSpace(message)
+}
+
+// Check date
+func currentLocalizedDate() (string, error) {
+	loc, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		return "", err
+	}
+	now := time.Now().In(loc)
+
+	localizedDate := fmt.Sprintf("%d月%d日", now.Month(), now.Day()-3) //TODO: minus 3 for debug purposes
+	return localizedDate, nil
 }
