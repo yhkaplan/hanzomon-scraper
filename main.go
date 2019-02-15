040a547f211c7cdd6888666761ba325a7364cd16
Joshua Kaplan <yhkaplan@gmail.com>
2019-02-15 20:58:58 +0900

    Refactor scraper functions into separate package

--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"strings"
-	"time"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/yhkaplan/scraper/scraper"
 )
 
 func main() {
@@ -24,41 +21,5 @@ func main() {
 		log.Fatal(err)
 	}
 
-	doc.Find("tr").Each(processTableRow)
-}
-
-func processTableRow(index int, tableRow *goquery.Selection) {
-	txt := tableRow.Text()
-	announce := announcementMessage(txt)
-
-	date, err := currentLocalizedDate()
-	if err != nil {
-		fmt.Printf("Fatal error: %s", err)
-		os.Exit(1)
-	}
-
-	isToday := strings.Contains(txt, date)
-	if isToday {
-		fmt.Printf("%s\n\n", announce)
-	}
-}
-
-// print text after 分, stripping newlines and whitespace
-func announcementMessage(txt string) string {
-	messageStartIdx := strings.Index(txt, "分") + 4
-	message := string([]byte(txt[messageStartIdx:]))
-
-	return strings.TrimSpace(message)
-}
-
-// Check date
-func currentLocalizedDate() (string, error) {
-	loc, err := time.LoadLocation("Asia/Tokyo")
-	if err != nil {
-		return "", err
-	}
-	now := time.Now().In(loc)
-
-	localizedDate := fmt.Sprintf("%d月%d日", now.Month(), now.Day()-3) //TODO: minus 3 for debug purposes
-	return localizedDate, nil
+	doc.Find("tr").Each(scraper.ProcessTableRow)
 }
---
+++ scraper/scraper.go
@@ -0,0 +1,46 @@
+package scraper
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func ProcessTableRow(index int, tableRow *goquery.Selection) {
+	txt := tableRow.Text()
+	announce := announcementMessage(txt)
+
+	date, err := currentLocalizedDate()
+	if err != nil {
+		fmt.Printf("Fatal error: %s", err)
+		os.Exit(1)
+	}
+
+	isToday := strings.Contains(txt, date)
+	if isToday {
+		fmt.Printf("%s\n\n", announce)
+	}
+}
+
+// print text after 分, stripping newlines and whitespace
+func announcementMessage(txt string) string {
+	messageStartIdx := strings.Index(txt, "分") + 4
+	message := string([]byte(txt[messageStartIdx:]))
+
+	return strings.TrimSpace(message)
+}
+
+// Check date
+func currentLocalizedDate() (string, error) {
+	loc, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		return "", err
+	}
+	now := time.Now().In(loc)
+
+	localizedDate := fmt.Sprintf("%d月%d日", now.Month(), now.Day()-3) //TODO: minus 3 for debug purposes
+	return localizedDate, nil
+}
